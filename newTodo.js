05e376efe47d4edd441bb3a1485aaf83fcdb1cf6
Ashutosh Yadav <yashutosh513@gmail.com>
2023-12-26 17:45:49 +0530

    Add files via upload

---
+++ newTodo.js
@@ -0,0 +1,79 @@
+function newTodo(value, completed = false) {
+  const todo = document.createElement('div');
+  const todoText = document.createElement('p');
+  const todoCheckBox = document.createElement('input');
+  const todoCheckBoxLabel = document.createElement('label');
+  const todoCross = document.createElement('span');
+
+  todoText.textContent = value;
+  todoCheckBox.type = 'checkbox';
+  todoCheckBox.id = `${value.replace(/\s+/g, '')}`;
+  todoCheckBox.name = `${value.replace(/\s+/g, '')}`;
+  todoCheckBox.title = 'checkbox';
+  todoCheckBox.checked = completed;
+  todoCheckBoxLabel.htmlFor = `${value.replace(/\s+/g, '')}`;
+
+  todoCheckBoxLabel.addEventListener('click', function (e) {
+    if (todoCheckBox.checked) {
+      todoText.classList.remove('strikethrough');
+      todoCheckBoxLabel.classList.remove('active');
+      updateTodos(value, false);
+      countCompleted();
+    } else {
+      updateTodos(value, true);
+      countCompleted();
+      todoText.classList.add('strikethrough');
+      todoCheckBoxLabel.classList.add('active');
+    }
+  });
+
+  todoText.addEventListener('click', function (e) {
+    if (todoCheckBox.checked) {
+      todoCheckBox.checked = false;
+      todoText.classList.remove('strikethrough');
+      todoCheckBoxLabel.classList.remove('active');
+      updateTodos(value, false);
+      countCompleted();
+    } else {
+      todoCheckBox.checked = true;
+      updateTodos(value, true);
+      countCompleted();
+      todoText.classList.add('strikethrough');
+      todoCheckBoxLabel.classList.add('active');
+    }
+  });
+
+  todoCross.addEventListener('click', function (e) {
+    e.target.parentElement.remove();
+    todos = todos.filter((t) => t.value !== value);
+    countCompleted();
+    if (todos.length === 0) {
+      updateUi(true);
+    }
+  });
+
+  todo.classList.add('todo');
+  todoCheckBoxLabel.classList.add('circle');
+  if (todoCheckBox.checked) {
+    todoCheckBoxLabel.classList.add('active');
+    todoText.classList.add('strikethrough');
+  }
+  todoCross.classList.add('cross');
+
+  todo.appendChild(todoCheckBox);
+  todo.appendChild(todoCheckBoxLabel);
+  todo.appendChild(todoText);
+  todo.appendChild(todoCross);
+
+  todosContainer.appendChild(todo);
+}
+
+function isBefore(elem1, el2) {
+  for (
+    var cur = elem1.previousSibling;
+    cur && cur.nodeType !== 9;
+    cur = cur.previousSibling
+  )
+    if (cur === el2) return true;
+  return false;
+}
